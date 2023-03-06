791883f5c82504e48bbd70e95408bdfc6794613c
22043810 <114679789+22043810@users.noreply.github.com>
2023-03-06 06:10:46 +0000

    Changes
    New repository created

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+Console.WriteLine("Hello, World!");
+public class Programming
+
+{
+	static void Main(string[] args)
+	{
+		double metersToMiles = 0.000621371;
+
+		double milesToFeet = 5280;
+
+		Console.Write("Type distance in meters:");
+		double meters = Convert.ToDouble(Console.ReadLine());
+
+		double miles = meters * metersToMiles;
+		Console.WriteLine("{0} meters is {1} miles", meters, miles);
+
+		Console.Write("Enter distance in miles: ");
+		double milesInput = Convert.ToDouble(Console.ReadLine());
+
+		double feet = milesInput * milesToFeet;
+		Console.WriteLine("{0} miles is {1} feet", milesInput, feet);
+	}
+}
