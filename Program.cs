dba923be2ce02123d1d2160a3a004f55e79d39b9
22043810 <22043810@bucks.ac.uk>
2023-03-06 07:45:18 +0000

    modification

--- Program.cs
+++ Program.cs
@@ -1,38 +1,45 @@
-﻿using ConsoleAppProject.App01;
-using ConsoleAppProject.App03;
-using ConsoleAppProject.Helpers;
-using System;
+﻿using System;
 
 namespace ConsoleAppProject
 {
 	/// <summary>
-	/// The main method in this class is called first
-	/// when the application is started.  It will be used
-	/// to start App01 to App05 for CO453 CW1
-	/// 
 	/// This Project has been modified by:
 	/// Mustafa Akram 05/03/2023
 	/// </summary>
-	class Program
-
-{
-	static void Main(string[] args)
+	public static class Program
 	{
+		public static void Main(string[] args)
+		{
+			Console.ForegroundColor = ConsoleColor.Yellow;
 
-		double metersToMiles = 0.000621371;
+			Console.WriteLine();
+			Console.WriteLine(" =================================================");
+			Console.WriteLine("    Distance Converter! By: Mustafa Akram ");
+			Console.WriteLine(" =================================================");
+			Console.WriteLine();
 
-		double milesToFeet = 5280;
+			double metersToMiles = 0.000621371;
 
-		Console.Write("Type distance in meters:");
-		double meters = Convert.ToDouble(Console.ReadLine());
+			double milesToFeet = 5280;
 
-		double miles = meters * metersToMiles;
-		Console.WriteLine("{0} meters is {1} miles", meters, miles);
+			Console.WriteLine("Please choose the unit by typing the assigned number:");
+			Console.WriteLine("1. Miles");
+			Console.WriteLine("2. Feet");
+			Console.WriteLine("3. Meters");
 
-		Console.Write("Enter distance in miles: ");
-		double milesInput = Convert.ToDouble(Console.ReadLine());
+			Console.Write("Type distance in meters:");
+			double meters = Convert.ToDouble(Console.ReadLine());
 
-		double feet = milesInput * milesToFeet;
-		Console.WriteLine("{0} miles is {1} feet", milesInput, feet);
+			double miles = meters * metersToMiles;
+			Console.WriteLine("{0} meters is {1} miles", meters, miles);
+
+			Console.Write("Enter distance in miles: ");
+			double milesInput = Convert.ToDouble(Console.ReadLine());
+
+			double feet = milesInput * milesToFeet;
+			Console.WriteLine("{0} miles is {1} feet", milesInput, feet);
+
+		}
+
 	}
 }
