5fe20b16a1738ee6e73a12416538a96b0a44fbe4
22043810 <22043810@bucks.ac.uk>
2023-03-06 06:52:26 +0000

    modification

--- Program.cs
+++ Program.cs
@@ -1,10 +1,24 @@
-﻿
-Console.WriteLine("Hello, World!");
-public class Programming
+﻿using ConsoleAppProject.App01;
+using ConsoleAppProject.App03;
+using ConsoleAppProject.Helpers;
+using System;
+
+namespace ConsoleAppProject
+{
+	/// <summary>
+	/// The main method in this class is called first
+	/// when the application is started.  It will be used
+	/// to start App01 to App05 for CO453 CW1
+	/// 
+	/// This Project has been modified by:
+	/// Mustafa Akram 05/03/2023
+	/// </summary>
+	class Program
 
 {
 	static void Main(string[] args)
 	{
+
 		double metersToMiles = 0.000621371;
 
 		double milesToFeet = 5280;
